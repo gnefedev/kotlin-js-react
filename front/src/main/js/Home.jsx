e3066b9ba64bf2cb25379e0fb56c597dab6698d9
Georgy Nefedev <gnefedev@yandex-team.ru>
2018-07-29 17:34:56 +0300

    formatting for article

--- front/src/main/js/Home.jsx
+++ front/src/main/js/Home.jsx
@@ -3,159 +3,266 @@ import {DataTable} from "primereact/components/datatable/DataTable";
 import {Column} from "primereact/components/column/Column";
 import {Dropdown} from "primereact/components/dropdown/Dropdown";
 
-class Home extends React.Component {
-    state = {
-        loaded: false,
-        color: searchAsMap(this.props.location.search)["color"],
-        brand: searchAsMap(this.props.location.search)["brand"]
-    };
+class Home
+  extends React.Component {
 
-    render() {
-        if (!this.state.loaded) return null;
-        return (
-            <Layout>
-                <Header>
-                    <HomeHeader
-                        brands={this.state.brands}
-                        brand={this.state.brand}
-                        onBrandChange={brand => this.navigateToChanged({brand})}
-                        colors={this.state.colors}
-                        color={this.state.color}
-                        onColorChange={color => this.navigateToChanged({color})}
-                    />
-                </Header>
-                <Content>
-                    <HomeContent cars={this.state.cars}/>
-                </Content>
-            </Layout>
+
+  state = {
+    loaded: false,
+    color: searchAsMap(
+      this.props.location.search
+    )["color"],
+    brand: searchAsMap(
+      this.props.location.search
+    )["brand"]
+  };
+
+
+  render() {
+    if (!this.state.loaded)
+      return null;
+    return (
+      <Layout>
+        <Header>
+          <HomeHeader
+    brands={this.state.brands}
+    brand={this.state.brand}
+    onBrandChange={brand =>
+  this.navigateToChanged({brand})
+    }
+    colors={this.state.colors}
+    color={this.state.color}
+    onColorChange={color =>
+  this.navigateToChanged({color})
+    }
+          />
+        </Header>
+        <Content>
+          <HomeContent
+    cars={this.state.cars}
+          />
+        </Content>
+      </Layout>
+    );
+  }
+
+  navigateToChanged({
+    brand = this.state.brand,
+    color = this.state.color
+  }) {
+    this.props.history.push(
+"?brand=" + (brand?brand:"")
++ "&color=" + (color?color:""))
+  }
+
+  async componentDidMount()
+  {
+    this.props.history.listen(
+      location => {
+        let query = searchAsMap(
+          location.search
         );
-    }
-
-    navigateToChanged({brand = this.state.brand, color = this.state.color}) {
-        this.props.history.push("/?brand=" + (brand ? brand : "") + "&color=" + (color ? color : ""))
-    }
-
-    async componentDidMount() {
-        this.props.history.listen(location => {
-            let query = searchAsMap(location.search);
-            this.setState({
-                brand: query["brand"],
-                color: query["color"]
-            });
-            this.loadData(query["brand"], query["color"])
-        });
         this.setState({
-            colors: await (await fetch('/api/colors')).json(),
-            brands: await (await fetch('/api/brands')).json()
+          brand: query["brand"],
+          color: query["color"]
         });
-        await this.loadData(this.state.brand, this.state.color);
-    }
 
-    async loadData(brand, color) {
-        let url = '/api/cars?brand=' + (brand ? brand : "") + "&color=" + (color ? color : "");
-        this.setState({
-            cars: await (await fetch(url)).json(),
-            loaded: true
-        });
-    }
+        this.loadData(
+          query["brand"],
+          query["color"]
+        )
+
+      });
+
+    this.setState({
+      brands: await (
+        await fetch('/api/brands')
+      ).json(),
+
+      colors: await (
+        await fetch('/api/colors')
+      ).json()
+
+    });
+
+    await this.loadData(
+      this.state.brand,
+      this.state.color
+    );
+  }
+
+
+  async loadData(
+    brand,
+    color
+  ) {
+    let url = '/api/cars?' +
+'brand=' + (brand?brand:"") +
+"&color=" + (color?color:"");
+    this.setState({
+      cars: await (
+        await fetch(url)
+      ).json(),
+
+      loaded: true
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
 }
 
 export default Home;
 
 //render part
-const HomeHeader = (
-    {
-        brands,
-        brand,
-        onBrandChange,
-        colors,
-        color,
-        onColorChange
-    }) => (
-    <div>
-        Brand:
-        <Dropdown
-            value={brand}
-            onChange={e => onBrandChange(e.value)}
-            options={withDefault("all", brands.map(value => ({label: value, value: value})))}/>
-        Color:
-        <Dropdown
-            value={color}
-            onChange={e => onColorChange(e.value)}
-            options={withDefault("all", colors.map(value => ({label: value, value: value})))}/>
-    </div>
+const HomeHeader = ({
+brands,
+brand,
+onBrandChange,
+colors,
+color,
+onColorChange
+}) => (
+  <div>
+    Brand:
+    <Dropdown
+      value={brand}
+      onChange={e =>
+        onBrandChange(e.value)
+      }
+      options={withDefault("all",
+    brands.map(value => ({
+      label: value, value: value
+    })))}
+
+    />
+    Color:
+    <Dropdown
+      value={color}
+      onChange={e =>
+        onColorChange(e.value)
+      }
+      options={withDefault("all",
+    colors.map(value => ({
+      label: value, value: value
+    })))}
+
+    />
+  </div>
 );
 
 HomeHeader.propTypes = {
-    brands: PropTypes.array.isRequired,
-    brand: PropTypes.string,
-    onBrandChange: PropTypes.func.isRequired,
-    colors: PropTypes.array.isRequired,
-    color: PropTypes.string,
-    onColorChange: PropTypes.func.isRequired
+  brands: PropTypes
+    .array.isRequired,
+  brand: PropTypes.string,
+  onBrandChange: PropTypes
+    .func.isRequired,
+  colors: PropTypes
+    .array.isRequired,
+  color: PropTypes.string,
+  onColorChange: PropTypes
+    .func.isRequired
 };
 
-const HomeContent = ({cars}) => (
-    <DataTable value={cars}>
-        <Column header="Brand" body={rowData => rowData["brand"]}/>
-        <Column header="Color" body={rowData => <span style={{color: rowData['color']}}>{rowData['color']}</span>}/>
-        <Column header="Year" body={rowData => rowData["year"]}/>
-    </DataTable>
+const HomeContent = ({
+ cars
+}) => (
+  <DataTable value={cars}>
+    <Column header="Brand"
+            body={rowData =>
+      rowData["brand"]
+            }/>
+    <Column header="Color"
+            body={rowData =>
+      <span
+    style={{
+      color: rowData['color']
+    }}>
+        {rowData['color']}
+      </span>
+  }/>
+    <Column header="Year"
+            body={rowData =>
+      rowData["year"]}
+    />
+  </DataTable>
 );
 
 HomeContent.propTypes = {
-    cars: PropTypes.array.isRequired,
+  cars: PropTypes
+    .array.isRequired,
 };
 
-
 //Layout
-const Layout = ({children}) => (
-    <div className={"wrapper"}>
-        {children}
-    </div>
+const Layout = ({
+  children
+}) => (
+  <div className={"wrapper"}>
+    {children}
+  </div>
 );
 
 Layout.propTypes = {
-    children: PropTypes.any.isRequired
+  children: PropTypes
+    .any.isRequired
 };
 
-const Header = ({children}) => (
-    <div className={"header"}>
-        {children}
-    </div>
+const Header = ({
+  children
+}) => (
+  <div className={"header"}>
+    {children}
+  </div>
 );
 
 Header.propTypes = {
-    children: PropTypes.object
+  children: PropTypes.object
 };
 
-const Content = ({children}) => (
-    <div className={"content"}>
-        {children}
-    </div>
+const Content = ({
+ children
+}) => (
+  <div className={"content"}>
+    {children}
+  </div>
 );
 
 Content.propTypes = {
-    children: PropTypes.object.isRequired
+  children: PropTypes
+    .object.isRequired
 };
 
 //infrastructure
 function searchAsMap(search) {
-    if (search !== undefined && search.length > 1) {
-        let result = {};
-        search.substr(1)
-            .split("&")
-            .map((pairStr) => pairStr.split("="))
-            .forEach((pair) => {
-                result[pair[0]] = pair[1]
-            });
-        return result
-    } else {
-        return {};
-    }
+  if (search !== undefined
+    && search.length > 1) {
+    let result = {};
+    search.substr(1)
+      .split("&")
+      .map((pairStr) =>
+        pairStr.split("="))
+      .forEach((pair) => {
+        result[pair[0]] = pair[1]
+      });
+    return result
+  } else {
+    return {};
+  }
 }
 
-function withDefault(label, options) {
-    options.unshift({label: label, value: null});
-    return options;
+function withDefault(
+  label, options
+) {
+  options.unshift({
+    label: label, value: null
+  });
+  return options;
 }
--- front/src/main/kotlin/com/gnefedev/react/version1/Home.kt
+++ front/src/main/kotlin/com/gnefedev/react/version1/Home.kt
@@ -21,163 +21,288 @@ import react.RState
 import react.dom.div
 import react.dom.span
 
-class Home(props: LocationProps) : RComponent<LocationProps, State>(props) {
-    init {
-        state = State(
-            loaded = false,
-            color = searchAsMap(props.location.search)["color"],
-            brand = searchAsMap(props.location.search)["brand"]
+class Home(props: LocationProps)
+  : RComponent
+<LocationProps, State>(props) {
+  init {
+    state = State(
+      loaded = false,
+      color = searchAsMap(
+        props.location.search
+      )["color"],
+      brand = searchAsMap(
+        props.location.search
+      )["brand"]
+    )
+  }
+
+  override fun
+    RBuilder.render() {
+    if (!state.loaded) return
+
+    layout {
+      header {
+        homeHeader(
+          brands = state.brands,
+          brand = state.brand,
+          onBrandChange = {
+  navigateToChanged(brand = it)
+          },
+          colors = state.colors,
+          color = state.color,
+          onColorChange = {
+  navigateToChanged(color = it)
+          }
         )
+      }
+      content {
+        homeContent(
+          cars = state.cars
+        )
+      }
     }
 
-    override fun RBuilder.render() {
-        if (!state.loaded) return
-        layout {
-            header {
-                homeHeader(
-                    brands = state.brands,
-                    brand = state.brand,
-                    onBrandChange = { navigateToChanged(brand = it) },
-                    colors = state.colors,
-                    color = state.color,
-                    onColorChange = { navigateToChanged(color = it) }
-                )
-            }
-            content {
-                homeContent(cars = state.cars)
-            }
-        }
-    }
+  }
 
-    private fun navigateToChanged(brand: String? = state.brand, color: String? = state.color) {
-        props.history.push("?brand=" + (brand ?: "") + "&color=" + (color ?: ""))
-    }
+  private fun navigateToChanged(
+    brand: String? = state.brand,
+    color: String? = state.color
+  ) {
+    props.history.push(
+"?brand=" + (brand ?: "")
++ "&color=" + (color ?: ""))
+  }
 
-    override fun componentDidMount() {
-        props.history.listen { location ->
-            val query = searchAsMap(location.search)
-            updateState {
-                brand = query["brand"]
-                color = query["color"]
-            }
-            launch {
-                loadData(query["brand"], query["color"])
-            }
-        }
-        launch {
-            updateState {
-                brands = fetchJson("/api/brands", StringSerializer.list)
-                colors = fetchJson("/api/colors", StringSerializer.list)
-            }
-
-            loadData(state.brand, state.color)
-        }
+  override fun componentDidMount()
+  {
+    props.history.listen {
+      location ->
+      val query = searchAsMap(
+        location.search
+      )
+      updateState {
+        brand = query["brand"]
+        color = query["color"]
+      }
+      launch {
+        loadData(
+          query["brand"],
+          query["color"]
+        )
+      }
     }
+    launch {
+      updateState {
+        brands = fetchJson(
+          "/api/brands",
+          StringSerializer.list
+        )
+        colors = fetchJson(
+          "/api/colors",
+          StringSerializer.list
+        )
+      }
 
-    private suspend fun loadData(brand: String?, color: String?) {
-        val url = "/api/cars?brand=" + (brand ?: "") + "&color=" + (color ?: "")
-        updateState {
-            cars = fetchJson(url, Car::class.serializer().list)
-            loaded = true
-        }
+      loadData(
+        state.brand,
+        state.color
+      )
     }
+  }
 
-    class State(
-        var loaded: Boolean,
-        var color: String?,
-        var brand: String?
-    ) : RState {
-        lateinit var cars: List<Car>
-        lateinit var brands: List<String>
-        lateinit var colors: List<String>
+  private suspend fun loadData(
+    brand: String?,
+    color: String?
+  ) {
+    val url = "/api/cars?" +
+"brand=" + (brand ?: "") +
+"&color=" + (color ?: "")
+    updateState {
+      cars = fetchJson(
+  url,
+  Car::class.serializer().list
+      )
+      loaded = true
     }
+  }
+
+  class State(
+    var loaded: Boolean,
+    var color: String?,
+    var brand: String?
+  ) : RState {
+lateinit var cars: List<Car>
+lateinit var brands: List<String>
+lateinit var colors: List<String>
+  }
+
 }
+
+
 
 //render part
 private fun RBuilder.homeHeader(
-    brands: List<String>,
-    brand: String?,
-    onBrandChange: (String?) -> Unit,
-    colors: List<String>,
-    color: String?,
-    onColorChange: (String?) -> Unit
+brands: List<String>,
+brand: String?,
+onBrandChange: (String?) -> Unit,
+colors: List<String>,
+color: String?,
+onColorChange: (String?) -> Unit
 ) {
-    +"Brand:"
-    dropdown(
-        value = brand,
-        onChange = onBrandChange,
-        options = brands.map { SelectItem(label = it, value = it) }.withDefault("all")
-    ) {}
-    +"Color:"
-    dropdown(
-        value = color,
-        onChange = onColorChange,
-        options = colors.map { SelectItem(label = it, value = it) }.withDefault("all")
-    ) {}
+
+  +"Brand:"
+  dropdown(
+    value = brand,
+    onChange = onBrandChange,
+
+
+    options = brands.map {
+      SelectItem(
+        label = it, value = it
+      )
+    } withDefault "all"
+  ) {}
+  +"Color:"
+  dropdown(
+    value = color,
+    onChange = onColorChange,
+
+
+    options = colors.map {
+      SelectItem(
+        label = it, value = it
+      )
+    } withDefault "all"
+  ) {}
+
 }
 
-private fun RBuilder.homeContent(cars: List<Car>) {
-    datatable(cars) {
-        column(header = "Brand") {
-            +it.brand
-        }
-        column(header = "Color") {
-            span {
-                attrs.style = js { color = it.color }
-                +it.color
-            }
-        }
-        column(header = "Year") {
-            +"${it.year}"
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+private fun RBuilder.homeContent(
+  cars: List<Car>
+) {
+  datatable(cars) {
+    column(header = "Brand") {
+
+      +it.brand
     }
+    column(header = "Color") {
+
+      span {
+        attrs.style = js {
+          color = it.color
+        }
+        +it.color
+      }
+    }
+    column(header = "Year") {
+
+      +"${it.year}"
+    }
+  }
 }
+
+
+
+
+
 
 //Layout
-private fun RBuilder.layout(children: RBuilder.() -> Unit) {
-    div(classes = "wrapper") {
-        children()
-    }
+private fun RBuilder.layout(
+  children: RBuilder.() -> Unit
+) {
+  div(classes = "wrapper") {
+    children()
+  }
 }
 
-private fun RBuilder.header(children: RBuilder.() -> Unit) {
-    div(classes = "header") {
-        children()
-    }
+
+
+
+
+
+private fun RBuilder.header(
+  children: RBuilder.() -> Unit
+) {
+  div(classes = "header") {
+    children()
+  }
 }
 
-private fun RBuilder.content(children: RBuilder.() -> Unit) {
-    div(classes = "content") {
-        children()
-    }
+
+
+
+
+private fun RBuilder.content(
+  children: RBuilder.() -> Unit
+) {
+  div(classes = "content") {
+    children()
+  }
 }
+
+
+
+
+
 
 //infrastructure
-external interface LocationProps : RProps {
-    var location: RLocation
+fun searchAsMap(search: String?) =
+  if (search != null
+    && search.length > 1) {
+
+    search.substring(1)
+      .split("&")
+      .map { it.split("=") }
+
+      .map { it[0] to it[1] }
+      .toMap()
+
+
+  } else {
+    emptyMap()
+  }
+
+infix fun <T : Any>
+  List<SelectItem<T>>.withDefault(
+  label: String
+) = listOf(
+  SelectItem(
+    label = label, value = null
+  )
+) + this
+
+external interface LocationProps
+  : RProps {
+  var location: RLocation
 }
 
 external interface RLocation {
-    var search: String?
+  var search: String?
 }
 
-val RProps.history: RHistory get() = this.asDynamic().history.unsafeCast<RHistory>()
+val RProps.history: RHistory get()
+= this.asDynamic().history
+    .unsafeCast<RHistory>()
 
 external interface RHistory {
-    fun push(path: String, state: Any? = definedExternally)
-    fun listen(listener: (RLocation) -> Unit)
+  fun push(
+    path: String,
+    state: Any? = definedExternally
+  )
+  fun listen(
+    listener: (RLocation) -> Unit
+  )
 }
-
-fun searchAsMap(search: String?): Map<String, String> =
-    if (search != null && search.length > 1) {
-        search.substring(1)
-            .split("&")
-            .map { it.split("=") }
-            .map { it[0] to it[1] }
-            .toMap()
-    } else {
-        emptyMap()
-    }
-
-infix fun <T: Any> List<SelectItem<T>>.withDefault(label: String) =
-    listOf(SelectItem(label = label, value = null)) + this
