1ea50a10191ca6d12fa7370b3f765e81f2bb9d6a
Georgy Nefedev <gnefedev@yandex-team.ru>
2018-11-11 18:23:49 +0300

    fix dependencies, small speedup of dev server

--- front/webpack.kotlin.js
+++ front/webpack.kotlin.js
@@ -15,7 +15,13 @@ module.exports = {
     },
     devtool: "inline-source-map",
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin()
     ],
     devServer: {
         contentBase: "./build/web/",
